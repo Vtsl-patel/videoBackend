a599541e053bf60aea36323158415ba45fdaecb1
Vtsl-patel <codephysicsvatsal@gmail.com>
2024-06-30 18:02:20 +0530

    dependencies added ; utility code written for standard response & error

--- src/index.js
+++ src/index.js
@@ -2,9 +2,22 @@ import mongoose from "mongoose"
 import dotenv from "dotenv"
 import DB_NAME from "constants"
 import connectDB from "./db/index.js"
+import app from "./app.js"
 
 dotenv.config({
     path: "./env"
 });
 
-connectDB();
+connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.log("Error : ", error);
+        throw error;
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.log("MongoDB connection error : ", error);
+})
